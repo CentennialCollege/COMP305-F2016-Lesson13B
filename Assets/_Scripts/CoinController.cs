60e48da4c5c79b19f6d215c22bad8337388614c7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-12-10 14:39:09 -0500

    Initial Commit

---
+++ Assets/_Scripts/CoinController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	// PUBLIC INSTANCE VARIABLES
+	public AudioSource CoinSound;
+
+
+	// PRIVATE INSTANCE VARIABLES
+	private WaitForSeconds _waitTime = new WaitForSeconds(0.15f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (Vector3.forward * Time.deltaTime * 360);
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			StartCoroutine (this.PlayCoinSound ()); // invoke coroutine
+		}
+	}
+
+	// Coroutine
+	IEnumerator PlayCoinSound() {
+		this.CoinSound.Play ();
+		yield return this._waitTime;
+		this.gameObject.SetActive (false); // disable game object
+	}
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// PUBLIC INSTANCE VARIABLES
+	public Transform SpawnPoint;
+	public GameObject Player;
+	public GameObject Coin;
+	public int MaxCoins = 20;
+
+
+	// PRIVATE INSTANCE VARIABLES
+
+	private List<GameObject> CoinPool;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (this.Player);
+		this.Player.transform.position = this.SpawnPoint.position;
+		this.CoinPool = new List<GameObject> (); // instantiate new List Container
+		this.BuildCoinPool();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.PlaceCoins ();
+	}
+
+	private void BuildCoinPool() {
+		for (int coinCount = 0; coinCount < this.MaxCoins; coinCount++) {
+			GameObject coin = (GameObject)Instantiate (this.Coin); // creates an instance of the Coin Prefab
+			coin.SetActive(false); // set coin to inactive / disabled
+			this.CoinPool.Add (coin);
+		}
+	}
+
+	private void PlaceCoins() {
+		foreach (var coin in CoinPool) {
+			if (!coin.activeInHierarchy) { // if coin is not in the scene
+				coin.SetActive(true);
+				coin.transform.position = new Vector3 (Random.Range (-20f, 20f), 20, Random.Range (-20f, 20f));
+			}
+		}
+	}
+}
